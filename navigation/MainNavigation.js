e425703451cfbeae0367ece20d1d1358e317e45a
ElianDeveloper <marincabreraeliandaniel@gmail.com>
2024-10-24 02:40:45 -0400

    feat: Modifying the App header

--- navigation/MainNavigation.js
+++ navigation/MainNavigation.js
@@ -1,20 +1,46 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { View, Text, TouchableOpacity, Image, StyleSheet } from "react-native";
 
-//Screens
+// Screens
 import MeetingScreen from "../screens/MeetingScreen";
 import TabsNavigation from "./TabsNavigation";
 import BenefitScreen from "../screens/BenefitScreen";
 
-//Init Stack
+// Init Stack
 const Stack = createNativeStackNavigator();
 
 export default function MainNavigation() {
   return (
     <NavigationContainer independent={true}>
       <Stack.Navigator
-        screenOptions={{ headerShown: false }}
+        screenOptions={{
+          headerShown: true,
+          headerStyle: {
+            backgroundColor: "#1f2937", // Fondo del header
+          },
+          headerTitle: () => (
+            <Text style={styles.headerTitle}>NailRoom</Text>
+          ),
+          headerTitleAlign: "center",
+          headerLeft: () => (
+            <TouchableOpacity style={{ marginLeft: 15 }}>
+              <Image
+                source={require("../assets/icons/home3.png")}
+                style={styles.icon}
+              />
+            </TouchableOpacity>
+          ),
+          headerRight: () => (
+            <TouchableOpacity style={{ marginRight: 15 }}>
+              <Image
+                source={require("../assets/icons/ingreso.png")}
+                style={styles.icon}
+              />
+            </TouchableOpacity>
+          ),
+        }}
         initialRouteName="Main"
       >
         <Stack.Screen name="Meetings" component={MeetingScreen} />
@@ -24,3 +50,17 @@ export default function MainNavigation() {
     </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  headerTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#FFFFFF", // Color del texto
+    fontFamily: "sans-serif-condensed", // Cambia esto a una fuente que prefieras
+  },
+  icon: {
+    width: 24,
+    height: 24,
+    tintColor: "#FFFFFF", // Asegúrate de que los íconos sean visibles
+  },
+});
