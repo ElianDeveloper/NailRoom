8b20be5f1245dd01e5d6708c0b2f8738dec3ee5e
ElianDeveloper <marincabreraeliandaniel@gmail.com>
2024-10-24 00:43:04 -0400

    feat: Testing DB in App.js component

--- App.js
+++ App.js
@@ -1,11 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from "react-native";
+import { SQLiteProvider, useSQLiteContext } from "expo-sqlite";
+import { init } from "./database/db";
+import { useEffect, useState } from "react";
+
+//Models
+import {
+  deleteMeeting,
+  fetchMeetings,
+  insertMeetings,
+} from "./database/models/Meetings";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Nail Room!</Text>
-      <StatusBar style="auto" />
+      <SQLiteProvider databaseName="test2.db" onInit={init}>
+        <Content />
+      </SQLiteProvider>
+    </View>
+  );
+}
+
+export function Content() {
+  const db = useSQLiteContext();
+  const [todos, setTodos] = useState([]);
+
+  async function setup() {
+    const result = await fetchMeetings(db);
+    setTodos(result);
+  }
+  useEffect(() => {
+    setup();
+  }, []);
+
+  async function insert() {
+    const currentDate = new Date().toISOString().split("T")[0];
+    insertMeetings(db, currentDate, "12:00:00");
+    setup();
+  }
+
+  async function deleteMeetingA(id) {
+    console.log("---> ", id);
+    deleteMeeting(db, id);
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+  }
+
+  return (
+    <View>
+      {todos.map((todo, index) => (
+        <View key={todo.id}>
+          <Text>{`${todo.id} - ${todo.meeting_date} - ${todo.meeting_time}`}</Text>
+          <Button
+            title="DeleteMeeting"
+            onPress={() => deleteMeetingA(todo.id)}
+          />
+        </View>
+      ))}
+      <Button title="Agregar" onPress={() => insert()} />
     </View>
   );
 }
@@ -13,8 +64,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
